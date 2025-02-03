74578acdb88c283b3d473ea4ac5f572ff0f88cc5
Kenward Terhemba <codewithkenward@gmail.com>
2025-02-03 07:53:57 +0100

    update: refactored the endpoint

--- number_api/urls.py
+++ number_api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('number_classifier.urls')),
+    path('api/', include('number_classifier.urls')),
 ]
